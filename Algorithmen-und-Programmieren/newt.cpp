615f8fb0e56e96fa861fd1a2e7ccea804b3096dd
kidyan0000 <756668972@qq.com>
2018-12-06 09:51:16 +0100

    12/6/2018

--- Algorithmen-und-Programmieren/newt.cpp
+++ Algorithmen-und-Programmieren/newt.cpp
@@ -8,6 +8,7 @@ void main()
 
 	double A_AS = 2., dm = 0.001, eps = 1.e-8;
 	bool is_supersonic = false;
+	double M, F, Mn, Fs, w;
 	int i=0;
 
 	if (is_supersonic == false)
@@ -17,15 +18,15 @@ void main()
 		do {
 
 			i++;
-			double M = i * dm;
-			double F = A_AS - 1. / M * pow(1. + 1. / 6.*(M*M - 1), 3);
+			M = i * dm;
+			F = A_AS - 1. / M * pow(1. + 1. / 6.*(M*M - 1), 3);
 
 
 		} while ( F<0.);
 		
-		double w = 1. + 1. / 6.*(M*M - 1);
-		double Fs = w * w * (1./(M*M)*w-1.);
-		double Mn = M - F / Fs;
+		w = 1. + 1. / 6.*(M*M - 1);
+		Fs = w * w * (1./(M*M)*w-1.);
+		Mn = M - F / Fs;
 		
 		M = Mn;
 		F = A_AS - 1. / M * pow(1. + 1. / 6.*(M*M - 1), 3);
