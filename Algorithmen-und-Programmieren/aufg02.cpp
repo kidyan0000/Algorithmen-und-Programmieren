3515a6bf905c0d6d019142f95d70d392c0f56389
kidyan0000 <756668972@qq.com>
2019-02-07 14:22:02 +0100

    Aufg02

---
+++ Algorithmen-und-Programmieren/aufg02.cpp
@@ -0,0 +1,51 @@
+// Augfgabe 02
+#include <stdio.h>
+#include <math.h>
+#include <cmath>
+
+void main()
+{
+	int    n = 10000; // set to 10000
+	double a = 1.0;
+	double b = 1.5;
+	double dx = (b - a)/(n - 1);
+	double pi = acos(0) * 2.;
+
+	double x0;
+	double y;
+	
+	x0 = 1.5;
+	/*
+	do
+	{
+		y = sin(pow(x0, 2) / 2.*pi);
+		x0 = x0 - dx;
+
+	} while (y < 0.); // stop until y<0
+	*/
+
+	double ds;
+	double s = x0;
+	
+	FILE *fin;
+	fopen_s(&fin, "aufg02.dat", "w");
+	int i = 0;
+	do
+	{
+		do
+		{
+			ds = -((sin(pow(s, 2) / 2.*pi)) / (s*cos(pow(s, 2) / 2. * pi)));
+			s = s + ds;
+			y = sin(pow(s, 2) / 2.*pi);
+			fprintf_s(fin, "%10.5lf %5.2f \n", y, s);
+			i++;
+		} while (abs(y) > 0.1); // use bigger than
+	} while (s > 1.5 || s < 1.0); // when not statisfied -> STOP
+								  // in our case should 1.0<s<1.5
+
+	fclose(fin);
+
+	printf("%10.5lf %5.2f %i", y, s,i);
+	scanf_s("%5.2lf",&y);
+	
+}
